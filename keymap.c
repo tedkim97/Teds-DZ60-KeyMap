b983136c28b06a67712b179920d8ac9017fd00bb
Ted Kim <tedkim97@gmail.com>
2020-09-09 15:42:00 -0500

    updates to the keyboard

--- keymap.c
+++ keymap.c
@@ -3,6 +3,7 @@
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
 #define ______ KC_TRNS
+#define XXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -16,21 +17,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------+
  * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
  * |-----------------------------------------------------------------------------------------+
- * | Ctrl |  Alt  |  Cmd  |              Space                |  Cmd |  FN |  L  |  D  |  R  |
+ * | CTRL |  GUI  |  ALT  |               Space                | FN2 | FN1 |  L  |  D  |  R  |
  * `-----------------------------------------------------------------------------------------'
  */
 
-  KEYMAP_2_SHIFTS(
-      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
+  LAYOUT_directional(
+      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, XXXXXX, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
+      KC_CAPSLOCK, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LSFT,    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
       KC_LCTL, KC_LGUI,KC_LALT , KC_SPC, KC_SPC, KC_SPC, MO(2), MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
 /* FN Layer
  * ,-----------------------------------------------------------------------------------------.
- * | `~ | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 |     |     |   RBGTOG   |
+ * | `~ | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |   RBGTOG   |
  * |-----------------------------------------------------------------------------------------+
  * |        |RBGM |RBGRM|     |     |     |     |     |     |     |     | Val+ | Val- |      |
  * |-----------------------------------------------------------------------------------------+
@@ -42,34 +43,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------'
  */
 
-  KEYMAP_DIRECTIONAL(
-      KC_GRV, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, ______, ______, ______, RGB_TOG,
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-      RESET, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,	
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_VOLU, KC_MUTE,
-      KC_SLEP, ______, ______, ______, ______, ______, ______, ______, ______, KC_VOLD, ______ 
+  LAYOUT_directional(
+      KC_GRV, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11, KC_F12, XXXXXX, RGB_TOG,
+      XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,
+      RESET, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,	
+      ______, ______, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, KC_VOLU, KC_MUTE,
+      KC_SLEP, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, KC_VOLD, XXXXXX 
       ),
 
   /* FN2 Layer
  * ,-----------------------------------------------------------------------------------------.
  * |    | BL  | BL_S| BL- | BL+ |     |     |RESET|     |     |     |     |     |   RBGTOG   |
  * |-----------------------------------------------------------------------------------------+
- * |        |LClick |RClick|    |    |    |     |     |     |     |     | Val+ | Val- |      |
- * |-----------------------------------------------------------------------------------------+
+ * |       |RGB_For|RGB_Bac|    |    |    |     |     |     |     |     | Val+ | Val- |      |
+ * |------------------------------------- ---------------------------------------------------+
  * |         | Hue+| Hue-| Sat+| Sat-|     |     |     |     |     |     |     |             |
  * |-----------------------------------------------------------------------------------------+
- * |           |     |     |     |     |     |     |     |     |     |     |     | MO+ |     |
+ * |           |BREATH|RNBOW|GRAD|SWIRL|STAT|KNIGHT|SNAKE|     |     |     |     |     |     |
  * |-----------------------------------------------------------------------------------------+
- * |      |       |       |                                   |     |      |L_MO | MO-| R_MO |
- * `-----------------------------------------------------------------------------------------'
+ * |      |       |       |                                   |     |PG_UP|PG_DN |     |     |
+ * `------------------------------------------------------------------------------------------
  */
 
-  KEYMAP_DIRECTIONAL(
-      ______, BL_TOGG, BL_STEP, BL_DEC, BL_INC, ______, ______, RESET, ______, ______, ______, ______, ______, ______, RGB_TOG,
-      ______, KC_BTN1, KC_BTN2, ______, ______, ______, ______, ______, ______, ______, ______, RGB_VAI, RGB_VAD, ______,
-      ______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, ______, ______, ______, ______, ______, ______, ______, 
-      ______, ______, RGB_M_B, RGB_M_R, RGB_M_G, RGB_M_SW, RGB_M_P, RGB_M_K, ______, ______, ______, ______, ______, KC_MS_U, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______, KC_MS_L, KC_MS_D, KC_MS_R 
+  LAYOUT_directional(
+      XXXXXX, BL_TOGG, BL_STEP, BL_DEC, BL_INC, XXXXXX, XXXXXX, RESET, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, RGB_TOG,
+      XXXXXX, RGB_MOD, RGB_RMOD, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, RGB_VAI, RGB_VAD, XXXXXX,
+      XXXXXX, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, 
+      ______, XXXXXX, RGB_M_B, RGB_M_R, RGB_M_G, RGB_M_SW, RGB_M_P, RGB_M_K, RGB_M_SN, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,
+      XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, KC_PGUP, KC_PGDOWN, XXXXXX, XXXXXX 
       ),
 };
 
--- readme.md
+++ readme.md
@@ -1,18 +1,15 @@
 # Keymap for DZ60 PCB
 Uses QMK to flash the firmware on the KB
 
-Layer 0(default) Keymap for JergOP DZ60:
+Layer 0(default) Keymap for Ted's DZ60:
 ![JergOP Layer 0 Keymap for XD60](https://i.imgur.com/z1h9NPs.png)
 
-Layer 1(FN 1) Keymap for JergOP DZ60:
-![](https://i.imgur.com/IMGusAx.png)
+Layer 1(FN 1) Keymap for Ted's DZ60:
+![](assets/main_layer.jpg)
 
 
-Layer 2(FN 2) Keymap for JergOP DZ60:
-![](https://i.imgur.com/b57quWf.png)
+Layer 2(FN 2) Keymap for Ted's DZ60:
+![](assets/fn1_layer.jpg)
 
 ## Build
 To build this keymap, insert the keymap into the correct directory, and run <make dz60:JergOP> 
-
-Final Build
-![](https://i.imgur.com/RxAlW0M.jpg?2)
